7b2d33453381120549313c14bb250aadc31aff73
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2008-07-26 15:08:35 +0000

    Started with implementation of map file handling

--- v0.20/src/datastorage.py
+++ v0.20/src/datastorage.py
@@ -1,12 +1,44 @@
 from dataprovider import Point
+import os
+
 
 class Waypoint(Point):
     def __init__(self,name='',lat=0,lon=0,alt=0):
         Point.__init__(self,lat,lon,alt)
         self.name = name
 
+
+class Refpoint(Point):
+    def __init(self,lat=0,lon=0,x=0,y=0):
+        Point.__init__(self,lat,lon)
+        self.x = x
+        self.y = y
+
+
+class FileSelector:
+    def __init__(self,dir=".",ext='.jpg'):
+        self.dir = dir
+        self.ext = ext
+        self.files = {}
+
+        def iter(fileselector,dir,files):
+            for file in files:
+                b,e = os.path.splitext(file)
+                if e == fileselector.ext:
+                    fileselector.files[u'%s' % b] = os.path.join(dir,file)
+
+        os.path.walk(self.dir,iter,self)
+
+
 class DataStorage:
     instance = None
+
+    def __init__(self):
+        self.maplist = None
+        self.tracklist = None
+        self.waypointlist = None
+        self.config = None
+
 
     def GetInstance():
         return DataStorage.instance
@@ -49,6 +81,16 @@ class DataStorage:
     def GetTracks(self):
         pass
 
+
+
+    def LoadMapConfig(self,file):
+        print "Loading map config: %s" % file
+
+    def ScanMaps(self,dir='.'):
+        print "Scanning maps in directory %s..." % dir
+        selector = FileSelector(dir,".xml")
+        for file in selector.files.values():
+            self.LoadMapConfig(file)
 
     def CreateMap(self,name=''):
         pass
