614b04758e4c59943d5913aa6a4af0e2098b680b
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2008-07-28 16:08:27 +0000

    Updates in GPX export functons

--- trunk/v0.20/src/posixdatastorage.py
+++ trunk/v0.20/src/posixdatastorage.py
@@ -1,4 +1,5 @@
 from datastorage import *
+import xml.etree.ElementTree as ET
 import dbm
 
 configlocations = [
@@ -12,6 +13,7 @@ configdefaults = {
         "mapdir":"~/.tracker/maps",
         "trackdir":"~/.tracker/tracks",
         "waypointfile":"~/.tracker/waypoints",
+        "gpxdir":"~/.tracker/gpx",
     }
 
 class PosixWaypoint(Waypoint):
@@ -38,5 +40,80 @@ class PosixDataStorage(DataStorage):
 
     def GetTrackFilename(self,name):
         return os.path.join(os.path.expanduser(self.config["trackdir"]),name+'.db')
+
+    def GetGPXFilename(self,name):
+        filename = os.path.join(os.path.expanduser(self.config["gpxdir"]),name+'.gpx')
+        print "GetGPXFilename: %s" % filename
+        return filename
+
+    def GPXExport(self,name):
+
+        def WriteTrackPoint(root,point):
+            lat,lon,alt = eval(point)
+            trkpt = ET.SubElement(root,"trkpt")
+            trkpt.set("lat",str(lat))
+            trkpt.set("lon",str(lon))
+            ele = ET.SubElement(trkpt,"ele")
+            ele.text = str(alt)
+
+        def WriteTrack(root,track):
+            trk = ET.SubElement(root,"trk")
+            name = ET.SubElement(trk,"name")
+            name.text = track.data["name"]
+            trkseg = ET.SubElement(trk,"trkseg")
+            for key in track.data.keys():
+                if key is not "name":
+                    try:
+                        WriteTrackPoint(trkseg,track.data[key])
+                    except:
+                        print "Unable to write trackpoint: %s:%s" % (key,track.data[key])
+
+        def WriteWaypoint(root,waypoint):
+            wpt = ET.SubElement(root,"wpt")
+            name = ET.SubElement(wpt,"name")
+            name.text = waypoint.name
+            wpt.set("lat",str(waypoint.latitude))
+            wpt.set("lon",str(waypoint.longitude))
+            ele = ET.SubElement(wpt,"ele")
+            ele.text = str(waypoint.altitude)
+
+        root = ET.Element('gpx')
+        #waypoints = self.GetWaypoints()
+        #for waypoint in waypoints:
+        #    WriteWaypoint(root,waypoint)
+
+        for track in self.tracks:
+            WriteTrack(root,track)
+
+        tree = ET.ElementTree(root)
+        tree.write(self.GetGPXFilename(name))
+
+
+    def GPXImport(self,name):
+        tree = ET.parse("newtrack.gpx")
+        root = tree.getroot()
+        if root.tag != "gpx":
+            print "No gpx tag found"
+            return
+
+        for c in root.getchildren():
+            print c.tag
+            if c.tag == "wpt":
+                print "Point: ", c.get("lat"), c.get("lon")
+                for s in c.getchildren():
+                    print s.tag
+                    if s.tag == "name":
+                        print "Waypoint name ", s.tag
+
+            if c.tag == "trk":
+                for s in c.getchildren():
+                    print s.tag
+                    if s.tag == "name":
+                        print "Track name ", s.tag
+                    if s.tag == "trkseg":
+                        for p in s.getchildren():
+                            print p.tag
+                            if p.tag == "trkpt":
+                                print "Point: ", p.get("lat"), p.get("lon")
 
 PosixDataStorage()
