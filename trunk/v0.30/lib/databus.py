7136d92af47eb8b8e2fa65bb43b2ad7c04af3836
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2008-10-28 21:54:00 +0000

    move timer and clock out

--- trunk/v0.30/lib/databus.py
+++ trunk/v0.30/lib/databus.py
@@ -1,15 +1,21 @@
+try:
+    from e32 import ao_sleep as sleep
+except:
+    from time import sleep
+
 from helpers import *
 loglevels += [ "databus!", "databus", "databus#", "databus*" ]
+
 
 
 class DataBus:
     def __init__(self):
         Log("databus","DataBus::__init__()")
+        import sys
+        plugindir = "../plugin"
+        sys.path.append(plugindir)
         self.subscriptions={}
-
-    def Quit(self):
-        Log("databus","DataBus::Quit()")
-        self.subscriptions={}
+        self.plugins = []
 
     def Connect(self,signal):
         Log("databus","DataBus::Connect(",signal,")")
@@ -49,87 +55,34 @@ class DataBus:
             for s in self.subscriptions[t].keys():
                 self.subscriptions[t][s](signal)
 
-class Clock:
-    def __init__(self,databus):
-        Log("databus","Clock::__init__()")
-        from time import time
-        self.bus = databus
-        self.bus.Signal( { "type":"connect",   "id":"clock", "signal":"clock", "handler":self.OnSignal } )
-        self.bus.Signal( { "type":"req_timer", "id":"clock", "interval":4, "start":time() } )
 
-    def OnSignal(self,signal):
-        Log("databus*","Clock::OnSignal(",signal,")")
-        print signal["time"]
+    def LoadPlugin(self,name):
+        import_string = "import %s as plugin" % name
+        exec import_string
+        self.plugins.append(plugin)
+        plugin.Init(self)
+        return len(self.plugins)-1
 
-    def Quit(self):
-        Log("databus","Clock::Quit()")
-        self.bus.Signal( { "type":"disconnect",   "id":"clock", "signal":"clock" } )
-        self.bus.Signal( { "type":"del_timer",    "id":"clock" } )
-        self.bus = None
-
-    def __del__(self):
-        Log("databus","Clock::__del__()")
-
-class Timer:
-    def __init__(self,databus):
-        Log("databus","Timer::__init__()")
-        self.bus = databus
-        self.requests = {}
-        self.running = True
-        self.bus.Signal( { "type":"connect", "id":"timer", "signal":"req_timer", "handler":self.OnRequest } )
-        self.bus.Signal( { "type":"connect", "id":"timer", "signal":"del_timer", "handler":self.OnDelete } )
-        import thread
-        thread.start_new_thread(self.Run,())
-
-    def OnRequest(self,signal):
-        Log("databus","Timer::OnRequest(",signal,")")
-        self.requests[signal["id"]]={"interval":signal["interval"], "start":signal["start"]}
-
-    def OnDelete(self,signal):
-        Log("databus","Timer::OnRequest(",signal,")")
-        del self.requests[signal["id"]]
-
-    def CheckForExpiredTimers(self):
-        import time
-        t = time.time()
-
-        for k in self.requests.keys():
-            r = self.requests[k]
-            if r["start"] + r["interval"] < t:
-                r["start"] += r["interval"]
-                self.bus.Signal( { "type":k, "time":t } )
-
-    def Run(self):
-        Log("databus","Timer::Run()")
-        from time import sleep
-        while self.running:
-            self.CheckForExpiredTimers()
-            sleep(1)
+    def UnloadPlugin(self,id):
+        self.plugins[id].Done()
+        del self.plugins[id]
 
     def Quit(self):
-        Log("databus","Timer::Quit()")
-        from time import sleep
-        self.running = False
-        sleep(1)
-        self.bus.Signal( { "type":"disconnect", "id":"timer", "signal":"req_timer" } )
-        self.bus.Signal( { "type":"disconnect", "id":"timer", "signal":"del_timer" } )
-        self.requests = {}
-        self.bus = None
-
-    def __del__(self):
-        Log("databus","Timer::__del__()")
-
+        Log("databus","DataBus::Quit()")
+        while len(self.plugins) > 0:
+            self.UnloadPlugin(-1)
+        self.subscriptions={}
 
 
 def Main():
     Log("databus","Main()")
-    from time import sleep
+
     b = DataBus()
-    t = Timer(b)
-    c = Clock(b)
+    for name in ["timer","clock"]:
+        b.LoadPlugin(name)
+
     sleep(20)
-    c.Quit()
-    t.Quit()
+
     b.Quit()
 
 Main()
