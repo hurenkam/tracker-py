0cb524442268c6c8b3ef2c16f4122a3e0f4770d9
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2008-10-29 23:26:52 +0000

    Added recorder plugin

--- v0.30/app/tracker.py
+++ v0.30/app/tracker.py
@@ -5,15 +5,24 @@ sys.path.append("../lib")
 from databus import *
 from helpers import *
 
+
+def StartRecording(b):
+    b.Signal( { "type":"start", "id":"recorder", "interval":10 } )
+
+def StopRecording(b):
+    b.Signal( { "type":"stop", "id":"recorder" } )
+
 def Main():
     from time import sleep
     Log("databus","Main()")
 
     b = DataBus()
-    for name in ["timer","clock","simgps"]:
+    for name in ["simgps","recorder"]:
         b.LoadPlugin(name)
 
+    StartRecording(b)
     sleep(20)
+    StopRecording(b)
 
     b.Quit()
 
