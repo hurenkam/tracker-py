784fb3984fa616077667dd627e4d4024977867fc
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2008-11-03 16:13:49 +0000

    Added waypoint display to wxmap. Prepared for track & route display.

--- trunk/v0.30/app/tracker.py
+++ trunk/v0.30/app/tracker.py
@@ -7,13 +7,16 @@ from helpers import *
 #loglevels += ["databus"]
 
 def StartRecording(b,name):
-    b.Signal( { "type":"trk_start", "interval":1, "name":name } )
+    b.Signal( { "type":"trk_start", "interval":10, "name":name } )
 
 def StopRecording(b):
     b.Signal( { "type":"trk_stop" } )
 
 def OpenMap(b,name):
-    b.Signal( { "type":"map_open", "name":name } )
+    b.Signal( { "type":"map_show", "name":name } )
+    
+def ShowWaypoint(b):
+    b.Signal( { "type":"wpt_show", "name":"Kampina", "latitude":51.5431429, "longitude":5.26938448, "altitude":0 } )
 
 def Main():
     import wx
@@ -26,6 +29,7 @@ def Main():
 
     StartRecording(b,"default")
     OpenMap(b,"51a_oisterwijk")
+    ShowWaypoint(b)
     app.MainLoop()
 
     StopRecording(b)
