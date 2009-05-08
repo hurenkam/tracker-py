7c7d72995da54559db70d71c9cbfc275d05b00aa
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2009-05-08 23:49:29 +0000

    Subscribe to position events, and respond with monitor events in case waypoint monitoring is enabled.

--- v0.30/lib/waypoints.py
+++ v0.30/lib/waypoints.py
@@ -12,6 +12,16 @@ class Waypoints:
         self.registry.Signal( { "type":"db_connect", "id":"wpt", "signal":"wpt_del",    "handler":self.OnWptDel } )
         self.registry.Signal( { "type":"db_connect", "id":"wpt", "signal":"wpt_search", "handler":self.OnWptSearch } )
         self.registry.Signal( { "type":"db_connect", "id":"wpt", "signal":"wpt_monitor","handler":self.OnWptMonitor } )
+        self.registry.ConfigAdd( { "setting":"wpt_monitor", "description":u"Waypoint to be monitored",
+                                   "default":None, "query":None } )
+        self.monitor = None
+        self.tolerance = None
+
+    def GetMonitor(self):
+        return self.registry.ConfigGetValue("wpt_monitor")
+
+    def SetMonitor(self,waypoint):
+        self.registry.ConfigSetValue("wpt_monitor",waypoint)
 
     def GetWaypoint(self,signal):
         pass
@@ -22,11 +32,10 @@ class Waypoints:
     def OnWptAdd(self,signal):
         Log("wpt","Waypoints::OnWptAdd(",signal,")")
         wpt = GetWaypoint(signal)
-        handle = self.WaypointAdd(wpt)
+        self.WaypointAdd(wpt)
         self.registry.Signal(self.GetSignal(wpt),
                 type="wpt_show",
-                id="wpt",
-                handle="handle"
+                id="wpt"
             )
 
     def OnWptDel(self,signal):
@@ -43,6 +52,26 @@ class Waypoints:
     def OnWptMonitor(self,signal):
         Log("wpt","Waypoints::OnWptMonitor(",signal,")")
         self.WaypointMonitor(self.GetWaypoint(signal))
+
+    def OnPosition(self,signal):
+        Log("wpt","Waypoints::OnPosition(",signal,")")
+        if self.monitor is None:
+            return
+
+        lat1,lon1 = (signal["latitude"],signal["longitude"])
+        lat2,lon2 = (self.monitor.latitude,self.monitor.longitude)
+        if lat1 != None and lon1 != None and lat2 != None and lon2 != None:
+            distance,bearing = datums.CalculateDistanceAndBearing( (lat1,lon1), (lat2,lon2) )
+
+            signal = {
+                "type":"monitor",
+                "id":"wpt",
+                "distance":distance,
+                "bearing":bearing
+            }
+
+            self.registry.Signal(signal)
+            Log("wpt","Waypoints::OnPosition(",signal,")")
 
     def SignalWaypointDone(self,ref):
         Log("wpt*","Waypoints::SignalWaypointDone()")
@@ -67,8 +96,28 @@ class Waypoints:
     def WaypointDel(self,waypoint):
         Log("wpt","Waypoints::WaypointDel()")
 
-    def WaypointSearch(self,area=None,categories=None):
+    def WaypointSearch(self,name=None,area=None,categories=None):
         Log("wpt","Waypoints::WaypointSearch()")
+
+    def WaypointMonitor(self,waypoint):
+        Log("wpt","Waypoints::WaypointMonitor()")
+        self.UnsubscribePositionSignals()
+        if waypoint is None:
+            self.SetMonitor(None)
+        else:
+            self.SubscribePositionSignals()
+            self.monitor = waypoint
+            self.SetMonitor(waypoint)
+
+    def SubscribePositionSignals(self):
+        Log("wpt","Waypoints::SubscribePositionSignals()")
+        self.registry.Signal( { "type":"db_connect", "id":"wpt", "signal":"position", "handler":self.OnPosition } )
+        self.registry.Signal( { "type":"gps_start",  "id":"wpt", "tolerance":None } )
+
+    def UnsubscribePositionSignals(self):
+        Log("wpt","Waypoints::UnsubscribePositionSignals()")
+        self.registry.Signal( { "type":"db_disconnect", "id":"wpt", "signal":"position" } )
+        self.registry.Signal( { "type":"gps_stop",      "id":"wpt" } )
 
     def Quit(self):
         Log("wpt","Waypoints::Quit()")
