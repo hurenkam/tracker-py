a5045cdc54cf50bbe190ea58690a54efcc5148bf
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2009-06-11 00:00:23 +0000

    Updates: - Now uses Property class for accessing RProperty - Now supports EByteArray as well as EInt - Added Buffer class which is used to pass data in and out of EByteArray properties - Property defines and ataches to property upon creation
    Todo:
    - Support for Subscribe
    - Support for ELargeByteArray
    - Support dict, list and tuple types in Buffer

--- properties/test.py
+++ properties/test.py
@@ -1,24 +1,34 @@
 from properties import *
 from e32 import ao_sleep as Sleep
 
-def Callback():
-    SubscribeInt(KPSUidHWRMPowerState,KHWRMChargingStatus,Callback)
-    print "Charging status: ", GetInt(KPSUidHWRMPowerState,KHWRMChargingStatus)
+# Generic functionality, this works as is on the emulator
+# For execution on device, change KUID to a valid value (SID of the calling program, not tested yet!)
+KUid = 1
+EKey1 = 1
+EKey2 = 2
 
-Callback()
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,1)
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,0)
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,1)
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,2)
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,3)
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,4)
-Sleep(5)
-SetInt(KPSUidHWRMPowerState,KHWRMChargingStatus,5)
-Sleep(5)
-print "Done!"
+buf_in = Buffer(128)
+buf_in.WriteString("Hello World!")
+buf_in.WriteInt32(127)
+buf_in.WriteString("This is me!")
+
+pbuf = Property(KUid,EKey1,EByteArray)
+pbuf.Set(buf_in)
+buf_out = pbuf.Get()
+print buf_out.ReadString()
+print buf_out.ReadInt32()
+print buf_out.ReadString()
+
+pint = Property(KUid,EKey2,EInt)
+pint.Set(2)
+print pint.Get()
+pint.Set(127)
+print pint.Get()
+
+# Get Battery info
+battery_status  = Property(KPSUidHWRMPowerState,KHWRMBatteryStatus,EInt)
+battery_level   = Property(KPSUidHWRMPowerState,KHWRMBatteryLevel,EInt)
+charging_status = Property(KPSUidHWRMPowerState,KHWRMChargingStatus,EInt)
+print battery_status.Get()
+print battery_level.Get()
+print charging_status.Get()
