b0d59708ae87c7214ec88087d6eb3dc5938043b2
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2009-07-01 20:51:01 +0000

    Prepare for server/client split.

---
+++ v0.35/plugin/datumrd.py
@@ -0,0 +1,39 @@
+from helpers import *
+loglevels += ["rd!"]
+
+def Init(r):
+    global registry
+    registry = r
+    registry.DatumAdd("RD",FormatRD,QueryRD)
+
+def Done():
+    global registry
+    registry.DatumDel("RD")
+
+def Wgs2RD(latitude,longitude):
+    import datums
+    return datums.GetRdFromWgs84(latitude,longitude)
+
+def RD2Wgs(rdx,rdy):
+    import datums
+    return datums.GetWgs84FromRd(rdx,rdy)
+
+def QueryRD((latitude,longitude)):
+    Log("rd","QueryRD()")
+    from osal import MessageBox, SimpleQuery
+
+    rdx,rdy = Wgs2RD(latitude,longitude)
+    rdx = SimpleQuery(u"RD X:","number",rdx)
+    if rdx == None:
+        return
+
+    rdy = SimpleQuery(u"RD Y:","number",rdy)
+    if rdy == None:
+        return
+
+    return RD2Wgs(rdx,rdy)
+
+def FormatRD((latitude,longitude)):
+    Log("rd*","FormatRD()")
+    rdx,rdy = Wgs2RD(latitude,longitude)
+    return (u"RD", u"X: %s" % rdx, u"Y: %s" % rdy )
