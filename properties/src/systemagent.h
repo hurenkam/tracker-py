6327478b53153036ffe69023e449cebd8831b1bf
mark.hurenkamp@xs4all.nl <mark.hurenkamp@xs4all.nl@ef54a0f8-d952-0410-8fc1-d1a2e12a3fa0>
2009-07-03 14:12:15 +0000

    Added wrapper class for SystemAgent constants

---
+++ properties/src/systemagent.h
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name		: properties.h
+ Author	  : Mark Hurenkamp
+ Copyright   : This software is licenced under GPL v2
+ Description : properties.h - Cproperties class header
+ ============================================================================
+ */
+
+// This file defines the API for properties.dll
+
+#ifndef __SYSTEMAGENT_H__
+#define __SYSTEMAGENT_H__
+
+#include "module.h"
+
+extern sConstant systemagent_constants[];
+extern PyTypeObject tSystemAgent;
+		
+#endif  // __SYSTEMAGENT_H__
